329d11b6eac2525001dc0057ab6c32538b3555d7
Radbuglet <10578060+Radbuglet@users.noreply.github.com>
2024-01-22 15:26:48 -0700

    Fix macro exporting; add concatenation support

--- src/symbolique-proc/src/lib.rs
+++ src/symbolique-proc/src/lib.rs
@@ -1,13 +1,16 @@
 use std::num::NonZeroU32;
 
-use proc_macro::{Delimiter, Group, Ident, Literal, TokenStream, TokenTree};
+use proc_macro::{Group, Ident, Literal, Punct, Spacing, TokenStream, TokenTree};
 use sha2::{Digest, Sha256};
+
+use litrs::Literal as PLiteral;
 
 #[proc_macro]
 pub fn __handle_magic(tokens: TokenStream) -> TokenStream {
     process_stream(
         tokens,
         &mut StreamProcessState {
+            rng: fastrand::u128(..),
             nz_id_gen: NonZeroU32::new(1).unwrap(),
         },
     )
@@ -15,6 +18,7 @@ pub fn __handle_magic(tokens: TokenStream) -> TokenStream {
 
 #[derive(Debug)]
 struct StreamProcessState {
+    rng: u128,
     nz_id_gen: NonZeroU32,
 }
 
@@ -31,16 +35,26 @@ fn process_stream(tokens: TokenStream, state: &mut StreamProcessState) -> TokenS
                 new_group.set_span(group.span());
                 out.extend([TokenTree::Group(new_group)]);
             }
-            TokenTree::Ident(ident) if ident.to_string() == "__SYMBOLIQUE_MAGIC_HASH" => {
-                let Some(TokenTree::Literal(lit)) = tokens.next().map(strip_empty_groups) else {
-                    panic!("expected literal after `__SYMBOLIQUE_MAGIC_HASH` directive");
-                };
+            TokenTree::Ident(ident)
+                if matches!(
+                    ident.to_string().as_str(),
+                    "__SYMBOLIQUE_MAGIC_HASH" | "__SYMBOLIQUE_MAGIC_HASH_RAND"
+                ) =>
+            {
+                let names = tokens
+                    .next()
+                    .expect("expected token after `__SYMBOLIQUE_MAGIC_HASH` directive");
+                let span = names.span();
 
                 let mut sha = Sha256::new();
-                sha.update(&lit.to_string());
+                if ident.to_string() == "__SYMBOLIQUE_MAGIC_HASH_RAND" {
+                    sha.update(u128::to_be_bytes(state.rng));
+                }
+                hash_tokens(names, &mut sha);
+
                 out.extend([TokenTree::Ident(Ident::new(
-                    &format!("sym_{:x}", sha.finalize()),
-                    lit.span(),
+                    &format!("symbolique_{:x}", sha.finalize()),
+                    span,
                 ))]);
             }
             TokenTree::Ident(ident)
@@ -55,6 +69,14 @@ fn process_stream(tokens: TokenStream, state: &mut StreamProcessState) -> TokenS
                     .checked_add(1)
                     .expect("generated too many symbols");
             }
+            TokenTree::Ident(ident) if ident.to_string() == "__SYMBOLIQUE_DOLLAR" => {
+                out.extend([TokenTree::Punct(Punct::new('$', Spacing::Alone))]);
+            }
+            TokenTree::Ident(ident) if ident.to_string() == "__SYMBOLIQUE_SKIP" => {
+                if let Some(next) = tokens.next() {
+                    out.extend([next]);
+                }
+            }
             token => out.extend([token]),
         }
     }
@@ -62,16 +84,46 @@ fn process_stream(tokens: TokenStream, state: &mut StreamProcessState) -> TokenS
     out
 }
 
-fn strip_empty_groups(t: TokenTree) -> TokenTree {
+fn hash_tokens(t: TokenTree, sha: &mut Sha256) {
     match t {
-        TokenTree::Group(group)
-            if group.delimiter() == Delimiter::None && {
-                let mut stream = group.stream().into_iter();
-                stream.next().is_some() && stream.next().is_none()
-            } =>
-        {
-            strip_empty_groups(group.stream().into_iter().next().unwrap())
+        TokenTree::Group(group) => {
+            for t in group.stream() {
+                hash_tokens(t, sha);
+            }
         }
-        t => t,
+        TokenTree::Ident(ident) => {
+            sha.update(ident.to_string().as_bytes());
+        }
+        TokenTree::Literal(lit) => {
+            let lit = PLiteral::parse(lit.to_string()).unwrap();
+            match lit {
+                PLiteral::Bool(lit) => {
+                    if lit.value() {
+                        sha.update(b"true");
+                    } else {
+                        sha.update(b"false");
+                    }
+                }
+                PLiteral::Integer(lit) => {
+                    sha.update(lit.raw_input());
+                }
+                PLiteral::Float(lit) => {
+                    sha.update(lit.raw_input());
+                }
+                PLiteral::Char(lit) => {
+                    sha.update(lit.value().to_string().as_bytes());
+                }
+                PLiteral::String(lit) => {
+                    sha.update(lit.value().as_bytes());
+                }
+                PLiteral::Byte(lit) => {
+                    sha.update([lit.value()]);
+                }
+                PLiteral::ByteString(lit) => {
+                    sha.update(lit.value());
+                }
+            }
+        }
+        TokenTree::Punct(_punct) => { /* ignored */ }
     }
 }
--- src/symbolique/src/lib.rs
+++ src/symbolique/src/lib.rs
@@ -5,7 +5,7 @@ pub mod macro_internals {
         std::{
             clone::Clone,
             cmp::{Eq, Ord, PartialEq, PartialOrd},
-            fmt,
+            concat, fmt,
             hash::Hash,
             marker::Copy,
             mem::transmute,
@@ -20,76 +20,93 @@ macro_rules! define_table {
     (
 		$(#[$attr:meta])*
 		$vis:vis struct $symbol:ident {
-			$($name:expr $(,)?)*
+			$($($name:literal)+),*
+			$(,)?
 		}
 	) => {
-		#[derive(
-			$crate::macro_internals::Copy,
-			$crate::macro_internals::Clone,
-			$crate::macro_internals::Hash,
-			$crate::macro_internals::Eq,
-			$crate::macro_internals::PartialEq,
-			$crate::macro_internals::Ord,
-			$crate::macro_internals::PartialOrd,
-		)]
-		$(#[$attr])*
-		#[repr(transparent)]
-		$vis struct $symbol(pub $crate::macro_internals::Spur);
+		$crate::macro_internals::__handle_magic! {
+			mod __SYMBOLIQUE_MAGIC_HASH(type $symbol) {
+				#[derive(
+					$crate::macro_internals::Copy,
+					$crate::macro_internals::Clone,
+					$crate::macro_internals::Hash,
+					$crate::macro_internals::Eq,
+					$crate::macro_internals::PartialEq,
+					$crate::macro_internals::Ord,
+					$crate::macro_internals::PartialOrd,
+				)]
+				$(#[$attr])*
+				#[repr(transparent)]
+				pub struct $symbol(pub $crate::macro_internals::Spur);
 
-		impl $crate::macro_internals::fmt::Debug for $symbol {
-			fn fmt(&self, f: &mut $crate::macro_internals::fmt::Formatter<'_>) -> $crate::macro_internals::fmt::Result {
-				$crate::macro_internals::fmt::Debug::fmt(Self::rodeo().resolve(&self.0), f)
-			}
-		}
+				impl $crate::macro_internals::fmt::Debug for $symbol {
+					fn fmt(&self, f: &mut $crate::macro_internals::fmt::Formatter<'_>) -> $crate::macro_internals::fmt::Result {
+						$crate::macro_internals::fmt::Debug::fmt(Self::rodeo().resolve(&self.0), f)
+					}
+				}
 
-		impl $crate::macro_internals::fmt::Display for $symbol {
-			fn fmt(&self, f: &mut $crate::macro_internals::fmt::Formatter<'_>) -> $crate::macro_internals::fmt::Result {
-				f.write_str(Self::rodeo().resolve(&self.0))
-			}
-		}
+				impl $crate::macro_internals::fmt::Display for $symbol {
+					fn fmt(&self, f: &mut $crate::macro_internals::fmt::Formatter<'_>) -> $crate::macro_internals::fmt::Result {
+						f.write_str(Self::rodeo().resolve(&self.0))
+					}
+				}
 
-		#[allow(dead_code)]
-		impl $symbol {
-			$crate::macro_internals::__handle_magic! {
-				$(
-					#[allow(non_upper_case_globals)]
-					pub const __SYMBOLIQUE_MAGIC_HASH $name: $symbol = unsafe {
-						// Safety: u32 -> NonZeroU32 -> Spur -> $symbol
-						$crate::macro_internals::transmute(__SYMBOLIQUE_GEN_NZ_INCREMENTAL_ID)
-					};
-				)*
-			}
-
-			pub fn new(str: &str) -> Self {
-				Self(Self::rodeo().get_or_intern(str))
-			}
-
-			pub fn as_str(&self) -> &str {
-				Self::rodeo().resolve(&self.0)
-			}
-
-			pub fn rodeo() -> &'static $crate::macro_internals::ThreadedRodeo {
-				static RODEO: $crate::macro_internals::OnceLock<$crate::macro_internals::ThreadedRodeo> =
-					$crate::macro_internals::OnceLock::new();
-
-				RODEO.get_or_init(|| {
-					let rodeo = $crate::macro_internals::ThreadedRodeo::new();
+				#[allow(dead_code)]
+				impl $symbol {
 					$(
-						// N.B. duplicates are already checked by the compiler because we hash each
-						// name to define a symbol for it.
-						rodeo.get_or_intern($name);
+						#[allow(non_upper_case_globals)]
+						pub const __SYMBOLIQUE_MAGIC_HASH($($name)*): $symbol = unsafe {
+							// Safety: u32 -> NonZeroU32 -> Spur -> $symbol
+							$crate::macro_internals::transmute(__SYMBOLIQUE_GEN_NZ_INCREMENTAL_ID)
+						};
 					)*
 
-					rodeo
-				})
-			}
-		}
+					pub fn new(str: &str) -> Self {
+						Self(Self::rodeo().get_or_intern(str))
+					}
 
-		#[allow(unused_macros)]
-		macro_rules! $symbol {
-			($name2:expr) => {
-				$crate::macro_internals::__handle_magic! { $symbol:: __SYMBOLIQUE_MAGIC_HASH $name2 }
-			};
+					pub fn as_str(&self) -> &str {
+						Self::rodeo().resolve(&self.0)
+					}
+
+					pub fn rodeo() -> &'static $crate::macro_internals::ThreadedRodeo {
+						static RODEO: $crate::macro_internals::OnceLock<$crate::macro_internals::ThreadedRodeo> =
+							$crate::macro_internals::OnceLock::new();
+
+						RODEO.get_or_init(|| {
+							let rodeo = $crate::macro_internals::ThreadedRodeo::new();
+							$(
+								// N.B. duplicates are already checked by the compiler because we hash each
+								// name to define a symbol for it.
+								rodeo.get_or_intern($crate::macro_internals::concat!($($name),*));
+							)*
+
+							rodeo
+						})
+					}
+				}
+			}
+
+			mod __SYMBOLIQUE_MAGIC_HASH(macro $symbol) {
+				#[macro_export]
+				#[allow(unused_macros)]
+				macro_rules! __SYMBOLIQUE_MAGIC_HASH_RAND(global_macro $symbol) {
+					( __SYMBOLIQUE_DOLLAR ($name2:literal)* ) => {
+						$crate::macro_internals::__handle_magic! { $symbol::__SYMBOLIQUE_SKIP __SYMBOLIQUE_MAGIC_HASH(
+							__SYMBOLIQUE_DOLLAR ( __SYMBOLIQUE_DOLLAR name2 )*
+						) }
+					};
+				}
+
+				#[allow(unused_imports)]
+				pub use __SYMBOLIQUE_MAGIC_HASH_RAND(global_macro $symbol) as $symbol;
+			}
+
+			#[allow(unused_imports)]
+			$vis use __SYMBOLIQUE_MAGIC_HASH(type $symbol)::*;
+
+			#[allow(unused_imports)]
+			$vis use __SYMBOLIQUE_MAGIC_HASH(macro $symbol)::*;
 		}
 	};
 }
